b65665be66e58ae989c35367feea7332b4e70da1
Fei Liu <Fei.Liu@gmail.com>
2017-09-27 14:29:15 +0000

    Update build script with hdf library path on theia

--- scripts/theia/build.sh
+++ scripts/theia/build.sh
@@ -1,7 +1,6 @@
 #! /bin/bash
 COMP=MOM6
-#COMP_SRCDIR=/scratch4/NCEPDEV/ocean/noscrub/Fei.Liu/UGCS-GSM-MOM6-CICE/MOM6
-COMP_SRCDIR=.
+COMP_SRCDIR=/scratch4/NCEPDEV/ocean/noscrub/Fei.Liu/MOM6_JD
 MACHINE_ID=theia
 COMP_BINDIR=INSTALL
 
@@ -11,7 +10,7 @@ module load intel impi/5.1.1.109 netcdf esmf/7.1.0bs07-g-IMPI5.1.1.109
 
 echo "Building $COMP..."
 cd $COMP_SRCDIR
-cp MOM6_scripts/compile.sh .
+cp scripts/${MACHINE_ID}/compile.sh .
 ./compile.sh --platform $MACHINE_ID
 cd ../MOM_CAP
 echo "make -f makefile.nuopc VERSION=mom6 USER_CFLAGS=-DMOM6_CAP NEMSMOMDIR=$COMP_SRCDIR/exec/$MACHINE_ID INSTALLDIR=$COMP_BINDIR install"
